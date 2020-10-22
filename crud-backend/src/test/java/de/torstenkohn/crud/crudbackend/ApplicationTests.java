b4fcc57d80ce0d3a8198ef5dff4ccc3165d4b4cd
Torsten Kohn <tkohn@users.noreply.github.com>
2020-10-22 19:08:25 +0200

    Create initial backend project
    Use Spring Initializr for setup

---
+++ crud-backend/src/test/java/de/torstenkohn/crud/crudbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.torstenkohn.crud.crudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

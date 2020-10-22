8a2dd74626ed51c64771f332df083c8637a25dab
Torsten Kohn <tkohn@users.noreply.github.com>
2020-10-22 21:10:15 +0200

    Fix package path

--- crud-backend/src/main/java/de/torstenkohn/crud/user/Application.java
+++ crud-backend/src/main/java/de/torstenkohn/crud/user/Application.java
@@ -1,4 +1,4 @@
-package de.torstenkohn.crud.crudbackend;
+package de.torstenkohn.crud.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
